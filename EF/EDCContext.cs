70085713f19c619c9b4a1adacac83bb6be5a9c9a
Steven Bitaxi <sbitaxi@gmail.com>
2020-07-25 08:16:21 -0400

    Add DbContext and Model

---
+++ EF/EDCContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFFunc_WithStartup.EF
+{
+    public class EDCContext : DbContext
+    {
+        public EDCContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ EF/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace EFFunc_WithStartup.EF
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+}
