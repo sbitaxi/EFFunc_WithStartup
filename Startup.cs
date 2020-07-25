89feeaf30cfa8b85daa4382b11be3b6b55986b59
Steven Bitaxi <sbitaxi@gmail.com>
2020-07-25 08:10:30 -0400

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(EFFunc_WithStartup.Startup))]
+
+namespace EFFunc_WithStartup
+{
+    class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
