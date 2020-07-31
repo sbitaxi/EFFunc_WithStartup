0aef4974d2409956a04e3ebe6b9db589f35cd06e
Steven Bitaxi <sbitaxi@gmail.com>
2020-07-30 21:43:17 -0400

    Separate Interface for DbContext

--- EF/IPerson.cs
+++ EF/IPerson.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,43 +7,5 @@ namespace EFFunc_WithStartup.EF
     public interface IPerson
     {
         Task<int> InsertPerson(Person person);
-    }
-
-    public class PersonService : IPerson
-    {
-        private readonly EDCContext _eDCContext;
-
-        public PersonService(EDCContext eDCContext)
-        {
-            _eDCContext = eDCContext;
-        }
-
-        public async Task<int> InsertPerson(Person person)
-        {
-            try
-            {
-                return await _eDCContext.Database.ExecuteSqlRawAsync(
-                @"EXEC AddNewPerson @FirstName = {0},
-                                @LastName = {1}, 
-                                @Email = {2}, 
-                                @Address = {3}, 
-                                @City = {4}, 
-                                @Province = {5},
-                                @PostalCode = {6}",
-                        person.FirstName,
-                        person.LastName,
-                        person.Email,
-                        person.Address,
-                        person.City,
-                        person.Province,
-                        person.PostalCode
-                        );
-
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
     }
 }
---
+++ EF/PersonService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace EFFunc_WithStartup.EF
+{
+    public class PersonService : IPerson
+    {
+        private readonly EDCContext _eDCContext;
+
+        public PersonService(EDCContext eDCContext)
+        {
+            _eDCContext = eDCContext;
+        }
+
+        public async Task<int> InsertPerson(Person person)
+        {
+            try
+            {
+                return await _eDCContext.Database.ExecuteSqlRawAsync(
+                @"EXEC AddNewPerson @FirstName = {0},
+                                @LastName = {1}, 
+                                @Email = {2}, 
+                                @Address = {3}, 
+                                @City = {4}, 
+                                @Province = {5},
+                                @PostalCode = {6}",
+                        person.FirstName,
+                        person.LastName,
+                        person.Email,
+                        person.Address,
+                        person.City,
+                        person.Province,
+                        person.PostalCode
+                        );
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
